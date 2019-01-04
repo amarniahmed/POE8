54bba5c5d6dd1ac5075f34beef3fc0282d0d1eba
ahmed <ahmed.amarni1985@gamil.com>
2019-01-04 11:42:47 +0100

    modif script deploy-mod

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/env bash
 
-#drush sset system.maintennance_mode 1
-#drush vset site_readonly
-#drush config-set readonlymode.settings enable 1 -y
+# Passage en mode maintenance.
+#drush sset system.maintenance_mode 1
+drush config-set readonlymode.settings enabled 1 -y
 echo Maintenance mode enabled.
 drush cr
 
+# Mise à jour code.
 git pull origin master
+
 composer install --no-dev
 drush cr
 
+#Mise à jour de base
 drush updb
 drush cr
 
+# Export des configs de prod.
 drush csex prod -y
 
+# import des configs.
 drush cim -y
 drush cr
 
+#Ajout des config de prod au master
 git add config/prod
-git commit -m 'Ajout de config de prod.'
-git log
+git commit -m 'Ajout des configs de prod.'
+git push origin master
 
-#drush sset system.maintennance_mode 0
-#drush config-set readonlymode.settings enable 0 -y
+# Sortie du mode de Maintenance.
+#drush sset system.maintenance_mode 0
+drush config-set readonlymode.settings enabled 0 -y
 drush cr
 echo Site is online.
